163730f00cc65e722aac20f33bbe57100c848612
Chintan-elite <thecsquare.elite@gmail.com>
2023-03-24 17:55:29 +0530

    d

---
+++ CoreJava/src/oop/BasicConcepts.java
@@ -0,0 +1,48 @@
+package oop;
+
+/*
+ * OOP (Object Oriented Programming)
+ * 
+ * There are 4 important pillars in OOP.
+ * 
+ * 1)Encapsulation 
+ * 2)Inheritance
+ * 3)Polymorphism
+ * 4)Abstraction 
+ * 
+ * 
+ * Encapsulation : It is wrapping of a class in a single unit.
+ * 
+ * e.g capsule
+ * 
+ * Class : It contains the data-members(variables) and member functions (methods,constructor,etc) which 
+ * 			does some behavioural change.
+ * 
+ * Object : It is an instance of the class.
+ * 
+ * Why do we create an object ?
+ * 
+ * -> In order to access the member-functions/data-members of the class, we create an object of 
+ *    the class.
+ *    
+ *    Lifecycle of an Object
+ *    
+ *    1) Creation
+ *    2) Utilization
+ *    3) Destruction
+ *    
+ *    
+ *    e.g a Papercup.*    
+ *    
+ *    
+ *    
+ * 
+ * 
+ * 
+ */
+
+
+
+public class BasicConcepts {
+
+}
